5ac069d573f7218d631617a27a44a88b69d3385e
Juzaweb <dangtheanh16@gmail.com>
2023-11-12 10:30:00 +0700

    :+1: Autoload services

---
+++ src/Providers/AutoloadServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\Subscription\Providers;
+
+use Juzaweb\CMS\Contracts\GlobalDataContract;
+use Juzaweb\CMS\Contracts\HookActionContract;
+use Juzaweb\CMS\Support\ServiceProvider;
+use Juzaweb\Subscription\Contrasts\PaymentMethodManager as PaymentMethodManagerContrast;
+use Juzaweb\Subscription\Contrasts\Subscription as SubscriptionContrast;
+use Juzaweb\Subscription\Repositories\PaymentMethodRepository;
+use Juzaweb\Subscription\Support\PaymentMethodManager;
+use Juzaweb\Subscription\Support\Subscription;
+
+class AutoloadServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->singleton(
+            PaymentMethodManagerContrast::class,
+            fn ($app) => new PaymentMethodManager(
+                $app[HookActionContract::class],
+                $app[GlobalDataContract::class],
+                $app[PaymentMethodRepository::class]
+            )
+        );
+
+        $this->app->singleton(
+            SubscriptionContrast::class,
+            Subscription::class
+        );
+    }
+}
