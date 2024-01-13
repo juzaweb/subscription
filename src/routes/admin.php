55f436a127cf4c9d6a74f356ee09d411c2f765c6
Juzaweb <dangtheanh16@gmail.com>
2024-01-13 14:06:20 +0700

    :+1: Remove PayPal old version

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -10,12 +10,10 @@
 |
 */
 
-use Juzaweb\Membership\Http\Controllers\Backend\UserSubscriptionController;
 use Juzaweb\Subscription\Http\Controllers\Backend\PaymentHistoryController;
 use Juzaweb\Subscription\Http\Controllers\Backend\PaymentMethodController;
 use Juzaweb\Subscription\Http\Controllers\Backend\PlanController;
 
 Route::jwResource('subscription/{module}/plans', PlanController::class);
 Route::jwResource('subscription/{module}/payment-methods', PaymentMethodController::class);
-Route::jwResource('subscription/{module}/subscriptions', UserSubscriptionController::class);
 Route::jwResource('subscription/{module}/payment-histories', PaymentHistoryController::class);
