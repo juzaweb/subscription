5e8b53af787b94de61125a1c9660571dba14b5d8
Juzaweb <dangtheanh16@gmail.com>
2024-01-14 15:31:26 +0700

    :bug: Fix migration

--- database/migrations/2023-11/2023_11_30_140550_add_site_id_to_tables.php
+++ database/migrations/2023-11/2023_11_30_140550_add_site_id_to_tables.php
@@ -30,23 +30,6 @@ return new class extends Migration {
         );
 
         Schema::table(
-            'subscription_user_subscriptions',
-            function (Blueprint $table) use ($prefix) {
-                $table->unsignedBigInteger('site_id')->index()->default(0);
-                $table->dropUnique("{$prefix}_user_subscriptions_module_user_unique");
-                $table->dropForeign("{$prefix}_user_subscription_plan_foreign");
-                $table->dropUnique("{$prefix}_user_subscriptions_plan_user_unique");
-
-                $table->unique(['module', 'user_id', 'site_id'], "{$prefix}_user_subscriptions_module_user_unique");
-                $table->unique(['plan_id', 'user_id', 'site_id'], "{$prefix}_user_subscriptions_plan_user_unique");
-
-                $table->foreign('plan_id', "{$prefix}_user_subscription_plan_foreign")
-                    ->references('id')
-                    ->on('subscription_plans');
-            }
-        );
-
-        Schema::table(
             'subscription_plans',
             function (Blueprint $table) {
                 $table->unsignedBigInteger('site_id')->index()->default(0);
@@ -72,13 +55,6 @@ return new class extends Migration {
             'subscription_payment_methods',
             function (Blueprint $table) {
                 $table->dropUnique(['site_id', 'method', 'module']);
-                $table->dropColumn(['site_id']);
-            }
-        );
-
-        Schema::table(
-            'subscription_user_subscriptions',
-            function (Blueprint $table) {
                 $table->dropColumn(['site_id']);
             }
         );
